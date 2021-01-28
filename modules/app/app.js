732638c212d94b2ae6be7dedbee465b667ec06c3
Esperanza Cardenas <esperanzacardenas@Esperanzas-MBP.home>
2021-01-27 20:14:09 -0800

    fixing typo

--- modules/app/app.js
+++ modules/app/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors())
 
 app.use('/', require('../people/people.router'));
-app.use('/', require('../pets.router'));
+app.use('/', require('../pets/pets.router'));
 
 app.get('/', (req, res) => {
     res.send('Hello Petful-Api');
